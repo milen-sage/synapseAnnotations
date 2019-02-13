ccb71f6f9f2b73858ed5e8423d6a4e952e65c0b6
Kara Woo <kara.woo@sagebase.org>
2019-02-13 10:37:44 -0800

    Fix output format

--- scripts/convert_to_json_schema.R
+++ scripts/convert_to_json_schema.R
@@ -36,12 +36,19 @@ create_schema_from_entry <- function(x) {
   ret
 }
 
-sage_comm <- read_json("../synapseAnnotations/data/sageCommunity.json", simplifyVector = FALSE)
+sage_comm <- read_json(
+  "../synapseAnnotations/data/sageCommunity.json",
+  simplifyVector = FALSE
+)
 
 definitions <- map(sage_comm, create_schema_from_entry) %>%
-  list("$schema" = "http://json-schema.org/draft-07/schema#",
-       "$id" = "http://example.com/definitions.json",
-       "definitions" = .) %>%
+  unlist(recursive = FALSE)
+
+output_json <- list(
+    "$schema" = "http://json-schema.org/draft-07/schema#",
+    "$id" = "http://example.com/definitions.json",
+    "definitions" = definitions
+  ) %>%
   toJSON(auto_unbox = TRUE, pretty = TRUE)
 
-write(definitions, "../synapseAnnotations/schemas/definitions.json")
+write(output_json, "../synapseAnnotations/schemas/definitions.json")
